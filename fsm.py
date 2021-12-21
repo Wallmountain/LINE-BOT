e202668fc05bc0972d60f355d5251b6cad593e8a
Wallmountain <Benny5090596@gmail.comls>
2021-12-21 23:51:48 +0800

    xxxxxxx xc

--- fsm.py
+++ fsm.py
@@ -7,30 +7,12 @@ class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_menu(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text.lower() == "go to menu"
 
-    def is_going_to_state2(self, event):
+    def is_going_to_introduce(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "go to introduce"
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
-        self.go_back()
-
-    def on_exit_state1(self):
-        print("Leaving state1")
-
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
-        self.go_back()
-
-    def on_exit_state2(self):
-        print("Leaving state2")
+    
